798163361b11eb78232d708c9fd32cab8be676a7
Vasyl Boroviak <kore.sar@gmail.com>
2017-02-02 17:03:45 +1100

    First implementation of Collision

--- packages/collision/__tests__/index.js
+++ packages/collision/__tests__/index.js
@@ -2,7 +2,55 @@ var compose = require('@stamp/compose');
 var Collision = require('..');
 
 describe('@stamp/collision', function () {
-  it('???', function () {
-    Collision.compose();
+  it('defer', function () {
+    var draw1 = jest.fn();
+    var Defer1 = compose({
+        methods: {
+          draw: draw1
+        }
+      },
+      Collision.setupCollision({defer: ['draw']})
+    );
+    var draw2 = jest.fn();
+    var Defer2 = Collision.setupCollision({defer: ['draw']}).compose({
+      methods: {
+        draw: draw2
+      }
+    });
+
+    var StampCombined = compose(Defer1, Defer2);
+    var obj = StampCombined();
+
+    obj.draw();
+
+    expect(draw1).toBeCalled();
+    expect(draw2).toBeCalled();
   });
+
+  it('forbid', function () {
+    var Forbid = compose({
+        methods: {
+          draw: function () {}
+        }
+      },
+      Collision.setupCollision({forbid: ['draw']})
+    );
+    var Defer = compose({
+        methods: {
+          draw: function () {}
+        }
+      },
+      Collision.setupCollision({defer: ['draw']})
+    );
+    var Allow = compose({
+        methods: {
+          draw: function () {}
+        }
+      },
+      Collision.setupCollision({allow: ['draw']})
+    );
+
+    expect(function () {compose(Forbid, Defer)}).toThrow();
+    expect(function () {compose(Forbid, Allow)}).toThrow();
+  })
 });
--- packages/collision/index.js
+++ packages/collision/index.js
@@ -1,34 +1,148 @@
 var compose = require('@stamp/compose');
 var isStamp = require('@stamp/is/stamp');
 var isObject = require('@stamp/is/object');
+var isArray = require('@stamp/is/array');
+
+function dedupe(array) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    var item = array[i];
+    if (result.indexOf(item) < 0) result.push(item);
+  }
+  return result;
+}
+
+function makeProxyFunction(functions, name) {
+  function deferredFn() {
+    'use strict';
+    for (var i = 0; i < functions.length; i++) {
+      functions[i].apply(this, arguments);
+    }
+  }
+
+  Object.defineProperty(deferredFn, 'name', {
+    value: name,
+    configurable: true
+  });
+
+  return deferredFn;
+}
+
+function getCollisionSettings(descriptor) {
+  return descriptor &&
+    descriptor.deepConfiguration &&
+    descriptor.deepConfiguration.Collision;
+}
+
+function descriptorHasSetting(descriptor, setting, methodName) {
+  var settings = getCollisionSettings(descriptor);
+  var settingsFor = settings && settings[setting];
+  return isArray(settingsFor) && settingsFor.indexOf(methodName) >= 0;
+}
+
+function forbidsCollision(descriptor, methodName) {
+  var settings = getCollisionSettings(descriptor);
+  if (settings && settings.forbidAll) return true;
+  return descriptorHasSetting(descriptor, 'forbid', methodName);
+}
+
+function defersCollision(descriptor, methodName) {
+  return descriptorHasSetting(descriptor, 'defer', methodName);
+}
 
 module.exports = compose({
-  deepConfiguration: {Privatize: {methods: []}},
+  deepConfiguration: {Collision: {defer: [], forbid: []}},
   staticProperties: {
-    methodCollisions: function () {
-      return this.compose({
-        deepConfiguration: {
-          Collision: {}
-        }
-      });
+    setupCollision: function (opts) {
+      return this.compose({deepConfiguration: {Collision: opts}});
+    },
+    resetCollisionSettings: function () {
+      return this.setupCollision(null);
+    },
+    protectFromAnyCollisions: function () {
+      return this.setupCollision({forbidAll: true});
     }
   },
   composers: [function (opts) {
-    var methods = opts.stamp.compose.methods;
-    var composables = opts.composables;
-    var methodsMetadata = {};
-    for (var i = 0; i < composables.length; i++) {
-      var c = composables;
-      c = isStamp(c) ? c.compose : c;
-      if (isObject(c.methods)) {
-        var methodNames = Object.keys(c.methods);
-        for (var j = 0; j < methodNames.length; j++) {
-          var name = methodNames[j];
+    var descriptor = opts.stamp.compose;
+    var settings = getCollisionSettings(descriptor);
 
+    var i, methodName;
+    if (settings) {
+      // Deduping is an important part of the logic
+      if (isArray(settings.defer)) {
+        settings.defer = dedupe(settings.defer);
+      }
+      if (isArray(settings.forbid)) {
+        settings.forbid = dedupe(settings.forbid);
+      }
 
+      // Make sure settings are not ambiguous
+      if (isArray(settings.defer) && isArray(settings.forbid)) {
+        for (i = 0; i < settings.forbid.length; i++) {
+          methodName = settings.forbid[i];
+          if (settings.defer.indexOf(methodName) >= 0) {
+            throw new Error('Ambiguous Collision settings. The `' + methodName +
+              '` is both deferred and forbidden');
+          }
         }
       }
     }
 
+    if (settings && (
+        isArray(settings.defer) && settings.defer.length > 0 ||
+        isArray(settings.forbid) && settings.forbid.length > 0
+      )) {
+      var d, j, oneMetadata;
+
+      var methodsMetadata = {}; // methods aggregation
+      var composables = opts.composables;
+      for (i = 0; i < composables.length; i++) {
+        d = composables[i];
+        d = isStamp(d) ? d.compose : d;
+        if (!isObject(d.methods)) continue;
+
+        var methodNames = Object.keys(d.methods);
+        for (j = 0; j < methodNames.length; j++) {
+          methodName = methodNames[j];
+          var method = d.methods[methodName];
+          if (!method) continue;
+
+          oneMetadata = methodsMetadata[methodName];
+          if (!oneMetadata) {
+            methodsMetadata[methodName] = method;
+            continue;
+          }
+
+          if (forbidsCollision(descriptor, methodName)) {
+            throw new Error('Collision of method `' + methodName +
+              '` is forbidden');
+          }
+
+          if (defersCollision(d, methodName)) {
+            var arr = isArray(oneMetadata) ? oneMetadata : [oneMetadata];
+            arr.push(method);
+            methodsMetadata[methodName] = arr;
+            continue;
+          }
+
+          methodsMetadata[methodName] = method;
+        }
+      }
+
+      var methods = opts.stamp.compose.methods;
+      var allMetadataMethods = Object.keys(methodsMetadata);
+      for (i = 0; i < allMetadataMethods.length; i++) {
+        methodName = allMetadataMethods[i];
+        oneMetadata = methodsMetadata[methodName];
+        if (isArray(oneMetadata) && oneMetadata.length > 1) {
+          // Some collisions aggregated to a single method
+          // Mutating the resulting stamp
+          methods[methodName] = makeProxyFunction(oneMetadata, methodName);
+        } else {
+          methods[methodName] = oneMetadata;
+        }
+      }
+    }
   }]
 });
