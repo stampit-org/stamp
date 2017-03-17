0aded11697632fb7cb16b4112b169152f4314a4a
Vasyl Boroviak <kore.sar@gmail.com>
2017-03-18 10:37:31 +1100

    Better tests for fp-constructor

--- packages/fp-constructor/__tests__/index.js
+++ packages/fp-constructor/__tests__/index.js
@@ -1,15 +1,20 @@
 var compose = require('@stamp/compose');
 var FpConstructor = require('..');
 
-describe('@stamp/fp-constructor', function () {
-  it('adds .constructor static method', function () {
+describe('@stamp/fp-constructor', function() {
+  it('adds .constructor should be the stamp itself', function() {
     var Stamp = compose(FpConstructor);
 
-    expect(typeof Stamp.constructor).toBe('function');
+    expect(Stamp.constructor).toBe(Stamp);
   });
 
-  it('adds .constructor should be the stamp itself', function () {
-    var Stamp = compose(FpConstructor);
+  it('.constructor presence regardless of composition chain', function() {
+    var Stamp = compose({staticProperties: {constructor: 1}})
+      .compose(
+        {staticProperties: {constructor: 2}},
+        FpConstructor,
+        {staticProperties: {constructor: 3}}
+      ).compose({staticProperties: {constructor: 4}});
 
     expect(Stamp.constructor).toBe(Stamp);
   });
