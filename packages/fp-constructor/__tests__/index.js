fd312ff74cbef4eee722f40f36f1f742c2aa19cb
Eric Elliott <support@paralleldrive.com>
2017-07-19 04:03:03 -1000

    [Fixes #35] DEPRECATION constructor/of broken

--- packages/fp-constructor/__tests__/index.js
+++ packages/fp-constructor/__tests__/index.js
@@ -1,20 +1,70 @@
 var compose = require('@stamp/compose');
 var FpConstructor = require('..');
 
-describe('@stamp/fp-constructor', function() {
-  it('adds .constructor should be the stamp itself', function() {
+describe('@stamp/fp-constructor Stamp.of', function() {
+  it('should add .of static method referring to the stamp', function() {
     var Stamp = compose(FpConstructor);
 
-    expect(Stamp.constructor).toBe(Stamp);
+    expect(Stamp.of).toBe(Stamp);
   });
 
-  it('.constructor presence regardless of composition chain', function() {
-    var Stamp = compose({staticProperties: {constructor: 1}})
+  it('should refer to the correct stamp regardless of other stamps in the composition', function() {
+    var Stamp = compose({staticProperties: {of: 1}})
       .compose(
-        {staticProperties: {constructor: 2}},
+        {staticProperties: {of: 2}},
         FpConstructor,
-        {staticProperties: {constructor: 3}}
-      ).compose({staticProperties: {constructor: 4}});
+        {staticProperties: {of: 3}}
+      ).compose({staticProperties: {of: 4}});
+
+    expect(Stamp.of).toBe(Stamp);
+  });
+});
+
+describe('@stamp/fp-constructor instance.constructor', function() {
+  it('should add .constructor method to the stamp', function() {
+    var Stamp = compose(FpConstructor);
+    var instance = Stamp();
+
+    expect(instance.constructor).toBe(Stamp);
+  });
+
+  it('should not be an own prop on the instance', function() {
+    var Stamp = compose(FpConstructor);
+    var instance = Stamp();
+
+    expect(Object.getPrototypeOf(instance).constructor).toBe(Stamp);
+  });
+
+  it('should refer to the correct stamp regardless of other stamps in the composition', function() {
+    var Stamp = compose({methods: {constructor: 1}})
+      .compose(
+        {methods: {constructor: 2}},
+        FpConstructor,
+        {methods: {constructor: 3}}
+      ).compose({methods: {constructor: 4}});
+    var instance = Stamp();
+
+    expect(instance.constructor).toBe(Stamp);
+  });
+
+  it('should not break other methods', function() {
+    var foo = function foo () {};
+    var bar = function bar () {};
+    var Stamp = compose({methods: {foo: foo}})
+      .compose(
+        FpConstructor
+      ).compose({methods: {bar: bar}});
+    var instance = Stamp();
+
+    expect(instance.foo).toBe(foo);
+    expect(instance.bar).toBe(bar);
+  });
+});
+
+
+describe('@stamp/fp-constructor Stamp.constructor (deprecated)', function() {
+  it('should add .constructor static method referring to the stamp', function() {
+    var Stamp = compose(FpConstructor);
 
     expect(Stamp.constructor).toBe(Stamp);
   });
--- packages/fp-constructor/index.js
+++ packages/fp-constructor/index.js
@@ -2,6 +2,10 @@ var compose = require('@stamp/compose');
 
 module.exports = compose({
   composers: [function (opts) {
+    opts.stamp.of = opts.stamp;
     opts.stamp.constructor = opts.stamp;
+    opts.stamp.compose.methods = Object.assign({}, opts.stamp.compose.methods, {
+      constructor: opts.stamp
+    });
   }]
 });
