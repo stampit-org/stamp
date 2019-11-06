c793b96a8247118d30fb382bf8fce5ed96cc9993
PopGoesTheWza <PopGoesTheWza@gmail.com>
2019-11-06 20:41:00 +0100

    refactor  for NodeJS 8

--- packages/named/__tests__/index.js
+++ packages/named/__tests__/index.js
@@ -1,28 +1,30 @@
-var compose = require('@stamp/compose');
-var Named = require('..');
+/* eslint-disable func-names */
+
+const compose = require('@stamp/compose');
+const Named = require('..');
 
 // This test can be executed by Jest testing framework only.
 // Its minimal node.js supported version is 4. Same as this stamp.
 // Thus, there is no risk running these test in a non-supported platform.
-describe('@stamp/named', function () {
-  it('default name is "Stamp"', function () {
-    var S = compose();
+describe('@stamp/named', function() {
+  it('default name is "Stamp"', function() {
+    const S = compose();
     expect(S.name).toBe('Stamp');
   });
 
-  it('setName sets the name', function () {
-    var S = compose(Named).setName('foo');
+  it('setName sets the name', function() {
+    const S = compose(Named).setName('foo');
     expect(S.name).toBe('foo');
   });
 
-  it('setName overwrites existing name', function () {
-    var S = compose(Named).setName('foo');
+  it('setName overwrites existing name', function() {
+    const S = compose(Named).setName('foo');
     expect(S.setName('bar').name).toBe('bar');
   });
 
-  it('can be used as a standalone function', function () {
-    var setName = Named.setName;
-    var S = setName('foo');
+  it('can be used as a standalone function', function() {
+    const { setName } = Named;
+    const S = setName('foo');
     expect(S.name).toBe('foo');
   });
 });
