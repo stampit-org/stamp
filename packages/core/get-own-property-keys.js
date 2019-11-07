71a2c70a5a2a72aa0376a9bacd3c3deaffc6e847
PopGoesTheWza <PopGoesTheWza@gmail.com>
2019-11-07 17:57:26 +0100

    refactor `core`

--- packages/core/get-own-property-keys.js
+++ packages/core/get-own-property-keys.js
@@ -1,5 +1,5 @@
-module.exports = function getOwnPropertyKeys(obj) {
-  var keys = Object.getOwnPropertyNames(obj);
-  if (!Object.getOwnPropertySymbols) return keys;
-  return keys.concat(Object.getOwnPropertySymbols(obj));
-};
+'use strict';
+
+const { ownKeys: getOwnPropertyKeys } = Reflect;
+
+module.exports = getOwnPropertyKeys;
