85d285f65ddbf2b06d14005a2d144b4b776905fb
PopGoesTheWza <PopGoesTheWza@gmail.com>
2019-11-08 10:26:24 +0100

    refactor and `use strict`

--- packages/convert-class/index.js
+++ packages/convert-class/index.js
@@ -1,55 +1,46 @@
-// 'use strict';
-
-// TODO: make `convertClass()` compatible with strict mode
+'use strict';
 
 const compose = require('@stamp/compose');
 
 const { prototype: functionPrototype } = Function;
+const { assign } = Object;
 const { construct, get, getPrototypeOf, ownKeys, set } = Reflect;
 
-function classStaticProperties(ctor) {
-  if (ctor === functionPrototype) return {};
-  return ownKeys(ctor).reduce((statics, k) => {
-    if (k !== 'length' && k !== 'name' && k !== 'prototype') {
-      set(statics, k, get(ctor, k));
-    }
-    return statics;
-  }, classStaticProperties(getPrototypeOf(ctor)));
-}
+const isClass = (value) => typeof value === 'function' && /^\s*class\s/.test(value.toString());
+const isFunction = (value) => value === functionPrototype;
 
-const classMethods = (ctor) => {
-  if (ctor === functionPrototype) return {};
-  const ctorPrototype = ctor.prototype;
-  return ownKeys(ctorPrototype).reduce((methods, k) => {
-    if (k !== 'constructor') {
-      set(methods, k, get(ctorPrototype, k));
-    }
-    return methods;
-  }, classMethods(getPrototypeOf(ctor)));
+const copyPropertiesFrom = (srcObj) => (destObj, key) => {
+  if (key !== 'length' && key !== 'name' && key !== 'prototype') set(destObj, key, get(srcObj, key));
+  return destObj;
 };
 
-const isClass = (v) => typeof v === 'function' && /^\s*class\s+/.test(v.toString());
+const classStaticProperties = (ctor) =>
+  isFunction(ctor) ? {} : ownKeys(ctor).reduce(copyPropertiesFrom(ctor), classStaticProperties(getPrototypeOf(ctor)));
 
-const convertClass = (ctor) => {
-  if (!isClass(ctor)) return compose();
-  return compose({
-    initializers: [
-      // eslint-disable-next-line func-names,no-unused-vars
-      function(_, { instance, args }) {
-        Object.assign(this, construct(ctor, args));
-      },
-    ],
-
-    methods: classMethods(ctor),
-
-    staticProperties: classStaticProperties(ctor),
-
-    staticPropertyDescriptors: {
-      name: {
-        value: ctor.name,
-      },
-    },
-  });
+const copyMethodsFrom = (srcObj) => (destObj, key) => {
+  if (key !== 'constructor') set(destObj, key, get(srcObj, key));
+  return destObj;
 };
+
+const classMethods = (ctor) =>
+  isFunction(ctor)
+    ? {}
+    : ownKeys(ctor.prototype).reduce(copyMethodsFrom(ctor.prototype), classMethods(getPrototypeOf(ctor)));
+
+const init = (ctor) =>
+  // eslint-disable-next-line func-names,no-unused-vars
+  function(_, { instance, args }) {
+    if (this) assign(this, construct(ctor, args));
+  };
+
+const convertClass = (ctor) =>
+  isClass(ctor)
+    ? compose({
+        initializers: [init(ctor)],
+        methods: classMethods(ctor),
+        staticProperties: classStaticProperties(ctor),
+        staticPropertyDescriptors: { name: { value: ctor.name } },
+      })
+    : compose();
 
 module.exports = convertClass;
