a1212723ff682fb902751773436947300a0444e7
PopGoesTheWza <PopGoesTheWza@gmail.com>
2019-11-12 11:18:40 +0100

    deprecate `getOwnPropertyKeys()`

--- packages/core/get-own-property-keys.js
+++ packages/core/get-own-property-keys.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/**
+ * @deprecated Use Reflect.ownKeys() instead
+ */
+
 const { ownKeys: getOwnPropertyKeys } = Reflect;
 
 module.exports = getOwnPropertyKeys;
--- packages/core/index.js
+++ packages/core/index.js
@@ -5,4 +5,5 @@
 module.exports = {
   assign: require('./assign'),
   merge: require('./merge'),
+  /** @deprecated Use Reflect.ownKeys() instead */ getOwnPropertyKeys: require('./get-own-property-keys'),
 };
