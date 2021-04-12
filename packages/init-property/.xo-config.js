9975f7fecba6d95248789961ac8676626e7534f4
PopGoesTheWza <popgoesthewza@gmail.com>
2021-04-12 18:27:09 +0200

    ci: move xo config to package.json

--- packages/init-property/.xo-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = require('../../.xo-config');
-const baseConfig = require('../../.xo-config.json');
-
-module.exports = {
-  ...baseConfig,
-  ...{
-    ignores: ['**.js', '**.d.ts', '**/__tests__'],
-  },
-};
--- packages/types/.xo-config.js
+++
@@ -1,8 +0,0 @@
-const baseConfig = require('../../.xo-config.json');
-
-module.exports = {
-  ...baseConfig,
-  ...{
-    ignores: ['**.js', '!**.d.ts', '**/__tests__', '**/check-compose'],
-  },
-};
